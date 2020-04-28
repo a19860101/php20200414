2b482bf6eb5768258192e38cc44031e1b78fe913
Zac <a19860101@gmail.com>
2020-04-28 20:29:14 +0800

    20200428

---
+++ db/create_student.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="name">姓名</label>
+                        <input type="text" id="name" name="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="phone">電話</label>
+                        <input type="text" id="phone" name="phone" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="mail">Mail</label>
+                        <input type="text" id="mail" name="mail" class="form-control">
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="新增">
+                    <input type="button" class="btn btn-danger" value="取消">
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
