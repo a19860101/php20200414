e9aca7816110a0ef3c863649654a73776004b7aa
Zac <a19860101@gmail.com>
2020-04-30 21:13:58 +0800

    20200430

--- db/create_student.php
+++ db/create_student.php
@@ -38,26 +38,26 @@
                     <div class="form-group">
                         <div>性別</div>
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" name="gender" id="male">
+                            <input type="radio" class="form-check-input" name="gender" id="male" value="男">
                             <label for="male" class="form-check-label">男性</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input type="radio" class="form-check-input" name="gender" id="female">
+                            <input type="radio" class="form-check-input" name="gender" id="female" value="女">
                             <label for="female" class="form-check-label">女性</label>
                         </div>
                     </div>
                     <div class="form-group">
                         <div>專長</div>
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" name="skills[]" id="graphic">
+                            <input type="checkbox" class="form-check-input" name="skills[]" id="graphic" value="平面設計">
                             <label for="graphic" class="form-check-label">平面設計</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" name="skills[]" id="web">
+                            <input type="checkbox" class="form-check-input" name="skills[]" id="web" value="網頁設計">
                             <label for="web" class="form-check-label">網頁設計</label>
                         </div>
                         <div class="form-check form-check-inline">
-                            <input type="checkbox" class="form-check-input" name="skills[]" id="threeD">
+                            <input type="checkbox" class="form-check-input" name="skills[]" id="threeD" value="3D設計">
                             <label for="threeD" class="form-check-label">3D設計</label>
                         </div>
                         
--- db/store_student.php
+++ db/store_student.php
@@ -3,7 +3,11 @@
     $name = $_POST["name"];
     $phone = $_POST["phone"];
     $mail = $_POST["mail"];
+    $edu = $_POST["edu"];
+    $gender = $_POST["gender"];
+    $skills = implode(",",$_POST["skills"]);
 
-    $sql = "INSERT INTO students(name,phone,mail)VALUES('$name','$phone','$mail')";
+
+    $sql = "INSERT INTO students(name,phone,mail,edu,gender,skills)VALUES('$name','$phone','$mail','$edu','$gender','$skills')";
     mysqli_query($conn,$sql);
     header("Location:index.php");
