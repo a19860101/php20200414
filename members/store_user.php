58179fd58acf66dc6caf72b38e9846bcfa5fcbd8
Zac <a19860101@gmail.com>
2020-05-12 22:04:30 +0800

    20200512

--- members/store_user.php
+++ members/store_user.php
@@ -1,13 +1,34 @@
 <?php
+    date_default_timezone_set("Asia/Taipei");
     try{
         include("pdo.php");
         $user = $_POST["user"];
         $pw = $_POST["pw"];
         $create_at = date("Y-m-d H:i:s");
-        $sql = "INSERT INTO members(user,pw,create_at,update_at)VALUES(?,?,?,?)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$user,$pw,$create_at,$create_at]);
-        header("Location:login.php");
+
+        $sql_check = "SELECT * FROM members WHERE user = ?";
+        $stmt_check = $pdo->prepare($sql_check);
+        $stmt_check->execute([$user]);
+
+        // $row_check = $stmt_check->fetch();
+        // if($row_check["user"] == $user){
+        //     echo "帳號已被使用";
+        // }
+
+        $row_count = $stmt_check->rowCount();
+        echo $row_count;
+        if($stmt_check->rowCount() > 0){
+            echo "帳號已被使用";
+        }else{
+            $sql = "INSERT INTO members(user,pw,create_at,update_at)VALUES(?,?,?,?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$user,$pw,$create_at,$create_at]);
+            header("Refresh:3;url=index.php");
+            echo "申請成功，請重新登入!";
+        }
+
+
+
     }
     catch(PDOException $e){
         echo $e->getMessage();
