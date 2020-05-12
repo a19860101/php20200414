2e142fc52f91d54d84a16a567c55aebc2fa1464e
Zac <a19860101@gmail.com>
2020-05-12 20:58:41 +0800

    20200512

---
+++ members/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="user">帳號</label>
+            <input type="text" name="user" id="user">
+        </div>
+        <div>
+            <label for="pw">密碼</label>
+            <input type="password" name="pw" id="pw">
+        </div>
+        <input type="submit" value="登入">
+    </form>
+</body>
+</html>
