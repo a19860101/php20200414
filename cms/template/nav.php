d1859263b8f96c40acd918679fb3d9524e1fc2ab
Zac <a19860101@gmail.com>
2020-05-14 19:27:42 +0800

    20200514

--- cms/template/nav.php
+++ cms/template/nav.php
@@ -1,3 +1,36 @@
-<nav>
-    主選單
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">QWERTY</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">首頁 <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">註冊</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">登入</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">會員專區</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">登出</a>
+      </li>
+    </ul>
+  </div>
 </nav>
