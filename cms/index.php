be41a5addd0c6c779a1dab4ced79a8bc2e87e6f3
Zac <a19860101@gmail.com>
2020-05-14 21:36:49 +0800

    20200514

--- cms/index.php
+++ cms/index.php
@@ -12,7 +12,10 @@
         <div class="col-8 py-2">
             <h3><?php echo $row["title"];?></h3>
             <div class="content">
-                <?php echo $row["content"];?>
+                <?php echo mb_substr($row["content"],0,120,"utf-8");?>...
+            </div>
+            <div class="text-right">
+                <a href="#">繼續閱讀</a>
             </div>
             <div class="mt-3">
                 發布時間: <?php echo $row["create_at"]; ?>
