557cdb2dc622ac30a778a82871e4940a39e93c87
Zac <a19860101@gmail.com>
2020-05-19 21:58:40 +0800

    20200519

--- cms/category.php
+++ cms/category.php
@@ -6,10 +6,26 @@
 ?>
 <?php include("template/header.php"); ?>
 <?php include("template/nav.php"); ?>
-<?php
-  foreach($rows as $row){
-      echo $row["title"];
-      echo "<br>";
-  }
-?>
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <?php foreach($rows as $row){ ?>
+        <div class="col-8 py-2">
+            <h3><?php echo $row["title"];?></h3>
+            <div class="content">
+                <?php echo mb_substr($row["content"],0,120,"utf-8");?>...
+            </div>
+            <div class="text-right">
+                <a href="post.php?id=<?php echo $row["id"];?>">繼續閱讀</a>
+            </div>
+            <div>
+                作者:<?php echo $row["user_id"];?>
+            </div>
+            <div class="mt-3">
+                發布時間: <?php echo $row["create_at"]; ?>
+            </div>
+            <hr>
+        </div>
+        <?php } ?>
+    </div>
+</div>
 <?php include("template/footer.php"); ?>
