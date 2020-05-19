d1c87c7a5d28fae551073e021493afa167fbc930
Zac <a19860101@gmail.com>
2020-05-19 19:53:40 +0800

    20200519

---
+++ cms/backend/function/category.php
@@ -0,0 +1,71 @@
+<?php
+    date_default_timezone_set("Asia/Taipei");
+    
+    function showAllCate($order="DESC"){
+        try {
+            global $pdo;
+            $sql = "SELECT * FROM category ORDER BY id {$order}";
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> execute(); 
+            $row_array = array();
+            while($rows = $stmt -> fetch()){
+                $row_array[] = $rows;
+            }
+            return $row_array;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    // function showPost($id){
+    //     try {
+    //         global $pdo;
+    //         $sql = "SELECT * FROM posts WHERE id = ?";
+    //         $stmt = $pdo->prepare($sql); 
+    //         $stmt->execute([$id]);
+    //         $row = $stmt->fetch();
+    //         return $row;
+    //     }catch(PDOException $e){
+    //         echo $e->getMessage();
+    //     }
+    // }
+    // function storePost($title,$content){
+    //     try{
+    //         global $pdo;
+    //         $sql = "INSERT INTO posts(title,content,create_at,update_at,cover)VALUES(?,?,?,?,?)";
+    //         $stmt = $pdo->prepare($sql);
+    //         $create_at = date("Y-m-d H:i:s");
+    //         $cover = 0;
+    //         $stmt->execute([$title,$content,$create_at,$create_at,$cover]);
+    //     }catch(PDOException $e){
+    //         echo $e->getMessage();
+    //     }
+    // }
+    // function deletePost($id){
+    //     try {
+    //         global $pdo;
+    //         $sql = "DELETE FROM posts WHERE id = ?";
+    //         $stmt = $pdo->prepare($sql);
+    //         $stmt->execute([$id]);
+    //     }catch(PDOException $e){
+    //         echo $e->getMessage();
+    //     }
+    // }
+    // function updatePost($title,$content,$id){
+    //     try{
+    //         global $pdo;
+    //         $update_at = date("Y-m-d H:i:s");            
+    //         $sql = "UPDATE 
+    //         posts 
+    //     SET 
+    //         title       = ?,
+    //         content     = ?,
+    //         update_at   = ?
+    //     WHERE 
+    //         id      = ?";
+    //     $stmt = $pdo->prepare($sql);
+    //     $stmt->execute([$title,$content,$update_at,$id]);
+    //     }catch(PDOException $e){
+    //         echo $e->getMessage();
+    //     }
+
+    // }
--- cms/template/nav.php
+++ cms/template/nav.php
@@ -1,3 +1,9 @@
+<?php 
+  require_once("backend/pdo.php");
+  include("backend/function/category.php");
+  $rows_c = showAllCate();
+
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">QWERTY</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,6 +14,11 @@
       <li class="nav-item active">
         <a class="nav-link" href="index.php">首頁 <span class="sr-only">(current)</span></a>
       </li>
+      <?php foreach($rows_c as $row_c){ ?>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo $row_c["title"];?></a>
+      </li>
+      <?php }?>
     </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
