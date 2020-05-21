230a14e04cbfb5e791865583ef27d14f5c2380cc
Zac <a19860101@gmail.com>
2020-05-21 21:55:59 +0800

    20200521

--- cms/register.php
+++ cms/register.php
@@ -3,6 +3,11 @@
     include("backend/function/member.php");
     $user = $_POST["user"];
     $pw = $_POST["pw"];
-    storeMember($user,$pw);
-    // echo "<script>alert('申請會員已完成，請重新登入')</script>";
-    header("Refresh:3;url=login.php");
+    if($user == "" || $pw == ""){
+        header("Location:index.php?user=0");
+        // return;
+    }else{
+        storeMember($user,$pw);
+        // echo "<script>alert('申請會員已完成，請重新登入')</script>";
+        header("Refresh:3;url=login.php");
+    }
