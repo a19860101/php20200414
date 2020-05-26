42e2345fc9bbc3395aaed295051893a7368fb389
Zac <a19860101@gmail.com>
2020-05-26 20:15:13 +0800

    20200526

--- cms/create-post.php
+++ cms/create-post.php
@@ -33,3 +33,5 @@
 <script>
     CKEDITOR.replace('content')
 </script>
+<!-- <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>tinymce.init({selector:'#content'});</script> -->
