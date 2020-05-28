28a552a71b50af11368faf898683cf1281a52f4b
Zac <a19860101@gmail.com>
2020-05-28 19:12:30 +0800

    20200528

---
+++ cms/page_test.php
@@ -0,0 +1,34 @@
+<?php 
+    require_once("backend/pdo.php");
+    include("backend/function/posts.php");
+    $rows = showAllPostsWithPage(2,'DESC');
+?>
+<?php include("template/header.php"); ?>
+<?php include("template/nav.php"); ?>
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <?php foreach($rows as $row){ ?>
+        <div class="col-8 py-2">
+            <h3><?php echo $row["title"];?></h3>
+            <div>
+                分類:<?php echo $row["c_title"];?>
+            </div>
+            <div class="content">
+                <?php echo mb_substr(strip_tags($row["content"]),0,120,"utf-8");?>...
+            </div>
+            <div class="text-right">
+                <a href="post.php?id=<?php echo $row["id"];?>">繼續閱讀</a>
+            </div>
+            <div>
+                作者:<?php echo $row["user"];?>
+            </div>
+            <div class="mt-3">
+                發布時間: <?php echo $row["create_at"]; ?>
+            </div>
+            <hr>
+        </div>
+        <?php } ?>
+    </div>
+</div>
+
+<?php include("template/footer.php"); ?>
